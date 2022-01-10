688ec7c866e6229685f16b56088116442b61b43b
Marcelo <mvinicius.louza@gmail.com>
2022-01-10 20:13:38 -0300

    Adicionar arquivos de projeto.

---
+++ Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace Employee_wage
+{
+    internal class Employee
+    {
+        public string Name;
+        public double BruteWage;
+        public double Tax;
+
+        public double LiquidWage()
+        {
+            return BruteWage - Tax;
+        }
+        public void IncraseWage(double percent)
+        {
+            BruteWage = BruteWage + (BruteWage * percent / 100.0);
+        }
+        public override string ToString()
+        {
+            return "Employee " + Name + " Liquid wage: US$ " + LiquidWage().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace Employee_wage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee func = new Employee();
+
+            Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-= Employee Wage =-=-=-=-=-=-=-=-=-=-=-=-=-");
+            Console.WriteLine("What is name of employee?");
+            Console.Write("Name: ");
+            func.Name = Console.ReadLine();
+
+            Console.WriteLine(" ");
+
+            Console.WriteLine("What is brute wage of employee?");
+            Console.Write("Brute Wage: USS$ ");
+            func.BruteWage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(" ");
+
+            Console.WriteLine("What is the tax?");
+            Console.Write("Tax: US$ ");
+            func.Tax = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+
+            Console.WriteLine(func);
+
+            Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+
+            Console.WriteLine("What the percent wo u want incrase?");
+            Console.Write("Percent: ");
+            double percent = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            func.IncraseWage(percent);
+
+            Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+
+            Console.WriteLine(func);
+        }
+    }
+}
